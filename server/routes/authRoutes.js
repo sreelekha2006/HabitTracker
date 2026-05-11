d31f0e07d0801d3fdb958f78dd715497d5317a1a
sreelekha2006 <sreelekhasivakumar2006@gmail.com>
2026-05-11 18:35:51 +0530

    authRoutes updated

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 
 const { registerUser, loginUser } = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Protected test route
+router.get("/profile", protect, (req, res) => {
+  res.status(200).json({
+    message: "Protected route accessed successfully",
+    user: req.user,
+  });
+});
 
 module.exports = router;
