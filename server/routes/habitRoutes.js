f947eb2f403320f8582ffb524eb1e66195209624
sreelekha2006 <sreelekhasivakumar2006@gmail.com>
2026-05-12 16:02:42 +0530

    habit routes updated

--- server/routes/habitRoutes.js
+++ server/routes/habitRoutes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
-const { createHabit, getHabits } = require("../controllers/habitController");
+const {
+  createHabit,
+  getHabits,
+  completeHabit,
+} = require("../controllers/habitController");
+
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", protect, createHabit);
+
 router.get("/", protect, getHabits);
+
+// Complete habit
+router.put("/:id/complete", protect, completeHabit);
 
 module.exports = router;
